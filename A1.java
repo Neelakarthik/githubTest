00494cf0fa1b95b342f5746af52ebd8dedd6d1a9
Neelakarthik <56343559+Neelakarthik@users.noreply.github.com>
2022-11-16 11:09:21 +0530

    Added New File

---
+++ A1.java
@@ -0,0 +1,18 @@
+package com.ashokit.corejava;
+
+public class A1 {
+
+	public A1() {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("A1 class constructor");
+	}
+	
+	public A1(int i) {
+		
+		// TODO Auto-generated constructor stub
+	}
+	public void m1() {
+		
+	}
+}
